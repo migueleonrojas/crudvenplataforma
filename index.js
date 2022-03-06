aeb8c877f7791614cbc810d734055a52a81c2688
migueleonrojas <migueleonrojas@gmail.com>
2022-03-06 10:17:53 -0400

    faltaba nodemon 2.0 sin jwt

--- index.js
+++ index.js
@@ -533,11 +533,11 @@ router.delete('/delete_company',(req, res) => {
 
 
 
-const port = 3000;
+/* const port = 3000;
 app.listen(port, () =>  {
 
     console.log(`El servidor esta corriendo en el puerto ${port}`);
 
-});
+}); */
 
 app.use(router);
